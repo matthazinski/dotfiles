5a0e6aa0fe08197f91aecf03462d44d53fdda4be
Matt Hazinski <matt@matthazinski.com>
2014-10-10 21:02:28 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+export UTILSREPO=https://github.com/matthazinski/utils.git
 export BASEDIR=$HOME/conf
 
 echo "Copying common files"
 cp -r $BASEDIR/.config $HOME
 cp $BASEDIR/.vimrc $HOME
 cp -r $BASEDIR/.vim $HOME
+cp $BASEDIR/.profile $HOME
 
 echo "Copying files for X11"
 cp $BASEDIR/.xinitrc $BASEDIR/.Xmodmap $BASEDIR/.wallpaper.jpg $HOME
 cp -r $BASEDIR/.i3 $HOME
+
+echo "Cloning utils repo"
+rm -r $HOME/utils
+git clone $UTILSREPO $HOME/utils
 
 # In the future this file will be more useful
 
