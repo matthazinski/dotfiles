13bf41baa1f08a54af1860c3466bb108af3c8b97
Matt Hazinski <matt@matthazinski.com>
2014-10-10 20:14:35 -0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-cp -r .* $HOME/
+echo "Copying common files"
+cp -r .config $HOME
+cp .vimrc $HOME
+cp -r .vim $HOME
+
+echo "Copying files for X11"
+cp .xinitrc .Xmodmap .wallpaper.jpg $HOME
+cp -r .i3 $HOME
 
 # In the future this file will be more useful
