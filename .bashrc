9b7599fc764b06cf0eea6dafd2ab5bb00c310bbf
Matt Hazinski <matt@matthazinski.com>
2014-11-09 04:07:08 -0500

    Added bash scripts

---
+++ .bashrc
@@ -0,0 +1,11 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific aliases and functions
+
+#. /home/matt/code/scripts/private/z/z.sh
+source .profile
--- install.sh
+++ install.sh
@@ -8,13 +8,15 @@ cp -r $BASEDIR/.config $HOME
 cp $BASEDIR/.vimrc $HOME
 cp -r $BASEDIR/.vim $HOME
 cp $BASEDIR/.profile $HOME
+cp $BASEDIR/.bashrc $HOME
+cp $BASEDIR/.bash_profile $HOME
 
 echo "Copying files for X11"
 cp $BASEDIR/.xinitrc $BASEDIR/.Xmodmap $BASEDIR/.wallpaper.jpg $HOME
 cp -r $BASEDIR/.i3 $HOME
 
 echo "Cloning utils repo"
-rm -r $HOME/utils
+rm -rf $HOME/utils
 git clone $UTILSREPO $HOME/utils
 
 # In the future this file will be more useful
